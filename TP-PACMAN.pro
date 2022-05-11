4738504669214bb12bf28d444024a64eda25d2c3
Nattthan <natthan34@outlook.com>
2022-05-11 23:14:53 +0200

    Fonction deplacer_personnage

--- TP-PACMAN.pro
+++ TP-PACMAN.pro
@@ -1,4 +1,5 @@
 QT       += core gui
+CONFIG += console
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
--- main.cpp
+++ main.cpp
@@ -1,11 +1,75 @@
 #include "mainwindow.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 #include <QApplication>
+#include <string>
 
-int main(int argc, char *argv[])
+bool deplacer_personnage(int x, int y, std::string cmd);
+void ExceptionBounds ();
+void ExceptionCommand ();
+
+int main(int argc, char** argv)
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    do {
+        string cmd;
+        std::getline(cin, cmd);
+        cout << "Vous avez saisi " << cmd << endl;
+        int x = 5, y = 4;
+        try {
+            deplacer_personnage(x, y, cmd);
+        }
+        catch (std::out_of_range& ExceptionBounds){
+            cout << "Vous allez dépasser les limites, faire un autre choix" << endl;
+        }
+        catch (std::string& ExceptionCommand){
+            cout << "La commande de déplacement n'est pas reconnu" << endl;
+        }
+
+        cout << "x= " << x << "\n" << "y= " << y << endl;
+        return 0;
+    }while (true);
+}
+
+bool deplacer_personnage(int x, int y, std::string cmd) {
+    const int xmin = 0;
+    const int xmax = 32;
+    const int ymin = 0;
+    const int ymax = 15;
+
+    if (cmd == "UP") {
+        y -= 1;
+        if (y < ymin) {
+            return false;
+        }
+        else return true;
+    }
+    else if (cmd == "DOWN") {
+        y += 1;
+        if (y > ymax) {
+            return false;
+        }
+        else return true;
+    }
+    else if (cmd == "LEFT") {
+        x -= 1;
+        if (x < xmin) {
+            return false;
+        }
+        else return true;
+    }
+    else if (cmd == "RIGHT") {
+        x += 1;
+        if (x > xmax) {
+            return false;
+        }
+        else return true;
+    }
+    else if (cmd == "IDLE") {
+        return false;
+    }
+    else return false;
+    
 }
